f15a87da3ff656d33472614ba143d627ede31ac5
Oliver Newland <onewland@gmail.com>
2009-10-03 16:26:31 -0400

    Added user support

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new 
+    @user = User.new 
+  end
+
+  def edit
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:notice] = "Registration successful."  
+      redirect_to '/'
+    else
+     render :action => 'new'
+    end
+  end
+
+end
